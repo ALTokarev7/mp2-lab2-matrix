b920a1d477801b3fb42370fd884ef089ee8e7755
ALTokarev7 <sasha_tokarev_02@mail.ru>
2022-11-07 17:52:12 +0300

    Готово

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -7,24 +7,84 @@
 #include <iostream>
 #include "tmatrix.h"
 //---------------------------------------------------------------------------
-
+void matAddMat()
+{
+	cout << "Введите размерность матрицы: ";
+	size_t sz = 0;
+	cin >> sz;
+	TDynamicMatrix<int> a(sz), b(sz), c(sz);
+	cout << "Введите первую матрицу: ";
+	cin >> a;
+	cout << "Введите вторую матрицу: ";
+	cin >> b;
+	cout << a << endl;
+	cout << "\t + " << "\n\n";
+	cout << b << endl;
+	cout << "\t = " << "\n\n";
+	c = a + b;
+	cout << c << endl;
+}
+void matMulScal()
+{
+	cout << "Введите размерность матрицы: ";
+	size_t sz = 0;
+	cin >> sz;
+	TDynamicMatrix<int> a(sz), c(sz);
+	cout << "Введите первую матрицу: ";
+	cin >> a;
+	int num = 0;
+	cout << "Введите число: ";
+	cin >> num;
+	cout << a << endl;
+	cout << "\t * " << "\n\n";
+	cout << "\t" << num << "\n\n";
+	cout << "\t = " << "\n\n";
+	c = a * num;
+	cout << c << endl;
+}
+void matMul()
+{
+	cout << "Введите размерность матрицы: ";
+	size_t sz = 0;
+	cin >> sz;
+	TDynamicMatrix<int> a(sz), b(sz), c(sz);
+	cout << "Введите первую матрицу: ";
+	cin >> a;
+	cout << "Введите вторую матрицу: ";
+	cin >> b;
+	cout << a << endl;
+	cout << "\t * " << "\n\n";
+	cout << b << endl;
+	cout << "\t = " << "\n\n";
+	c = a * b;
+	cout << c << endl;
+}
 void main()
 {
-  TDynamicMatrix<int> a(5), b(5), c(5);
-  int i, j;
-
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование класс работы с матрицами"
-    << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
-    {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
-    }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
+	setlocale(LC_ALL, "Russian");
+	cout << "Тестирование класс работы с матрицами"
+		<< endl;
+	cout << "Выберите операцию (введите номер): " << endl;
+	cout << "1.Матрица * матрица" << endl;
+	cout << "2.Матрица + матрица" << endl;
+	cout << "3.Матрица * число" << endl;
+	
+	size_t choose = 0;
+	cin >> choose;
+	switch (choose)
+	{
+	case 1:
+		matMul();
+		break;
+	case 2:
+		matAddMat();
+		break;
+	case 3:
+		matMulScal();
+		break;
+	default:
+		cout << "Неверный номер!";
+		break;
+	}
 }
 //---------------------------------------------------------------------------
